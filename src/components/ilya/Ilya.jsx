93ef8034a958a86af09ca336834b021f5eb9b406
Maxim <dylikmax899@gmail.com>
2025-05-06 16:12:27 +0300

    change size

--- src/components/ilya/Ilya.jsx
+++ src/components/ilya/Ilya.jsx
@@ -2,7 +2,7 @@ import "./Ilya.css"
 
 export default function Ilya({happiness}) {
   return <div className="ilya">
-    <img src="png/mouth.png" className={`mouth ${happiness >= 40 ? 'smiled' : 'sad'}`}/>
     <img src="png/gornak.png" className="ilya-img"/>
+    <img src="png/mouth.png" className={`mouth ${happiness >= 40 ? 'smiled' : 'sad'}`}/>
   </div>
 }
